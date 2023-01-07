069a89a534edea06be9f6ea7d400a1820015c051
Vikas Yadav <vvikasyadav95@gmail.com>
2023-01-07 20:53:44 +0530

    first-commit

--- script.js
+++ script.js
@@ -8,20 +8,57 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  arr.map((val)=>{
+   
+    if(val.profession === "developer"){
+      console.log(val);
+    }
+  })
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach((myfun)=>{
+    if(myfun.profession === "developer"){
+      console.log(myfun);
+    }
+  })
 }
 
 function addData() {
   //Write your code here
+  let newArr = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newArr);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here
+  //first-method
+  // for(var i=0;i<=arr.length;i++)
+  // {
+  //  if (arr[i].profession === "admin") {
+  //   arr.splice(i,1)
+    
+  //  }
+  //  console.log(arr); 
+  // }
+  //Second-method
+  arr.map((rem)=>{
+    if(rem.profession!=='admin'){
+      console.log(rem);
+    }
+  })
+
 }
 
 function concatenateArray() {
   //Write your code here
+  let secArr = [
+    { id: 4, name: "Avneeshwa", age: "26", profession: "BackendDeveloper" },
+    { id: 5, name: "Prashantwa", age: "20", profession: "SarakriPFAC" },
+    { id: 6, name: "Arifwa", age: "23", profession: "NOCEngineer" },
+  ];
+  let concatArr = arr.concat(secArr);
+  console.log(concatArr);
 }
